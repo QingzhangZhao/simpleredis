df7744104a65428113cb9e784e3306a14bb14720
QingzhangZhao <u201414852@hust.edu.cn>
2015-12-13 00:35:54 +0800

    add list

---
+++ list.h
@@ -0,0 +1,57 @@
+typedef struct listNode{
+
+	//the previos listnode
+	struct listNode *prev;
+
+	//the next listnode
+	struct listNode *next;
+
+	//the value 
+	void *value;
+}listNode;
+
+
+typedef struct list{
+      
+	listNode *head;
+
+	listNode *tail;
+
+	int len;
+}list;
+
+//create the first listnode
+list * cmd_list_init(char *value);
+
+void cmd_list_append(list *head,char *value);
+
+
+void cmd_list_lpush(list *head,char *value);
+
+
+
+
+void * cmd_list_lget(list *head);
+
+void * cmd_list_rget(list *head);
+
+void cmd_list_rpop(list *head);
+
+void cmd_list_lpop(list *head);
+
+int  cmd_list_llen(list *head);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
