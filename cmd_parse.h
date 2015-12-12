471bd61778ffb0cf0a72b0cebc3b0272a44343c2
QingzhangZhao <u201414852@hust.edu.cn>
2015-12-13 00:34:29 +0800

    command parser

---
+++ cmd_parse.h
@@ -0,0 +1,26 @@
+list * recv_parse(char *command);
+ static char *commandtable[] ={
+	 "set",      
+	 "get",
+	 "incr",
+	 "decr",
+	 "incrby",
+	 "decrby",
+	 "strlen",
+	 "del",
+	 "exists",
+	 "randomkey",
+	 "keys",
+	 "type",
+	 "append",
+	 "mesetnx",
+	 "setrange",
+	 "getrange",
+	 "lpush",
+	 "rpush",
+	 "lpop",
+	 "rpop",
+	 "llen"
+ };
+char *  cmd_parse(hashTable *table,list *cmd_list_head);
+
